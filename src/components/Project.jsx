4e35394e56f8dd22220e63523b0f85a978e9fa59
Aashish Katila <aashishkatila@gmail.com>
2023-11-16 22:22:07 +0545

    replaced 1 project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -163,16 +163,16 @@ const Project = () => {
             {/* Hover Effect */}
             <div className="opacity-0 group-hover:opacity-100  ">
               <span className="text-2xl font-bold text-white tracking-wider ">
-                Dancing Paragraph
+                Typing Speed Test
               </span>
 
               <div className="pt-8 text-center ">
-                <a href="https://candid-semifreddo-ddd50c.netlify.app/" target="_blank">
+                <a href="https://frabjous-kataifi-8aa65e.netlify.app/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/AashishKatila/Dancing-Paragraph" target="_blank">
+                <a href="https://github.com/AashishKatila/Typing-Speed-Test" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
                     Code
                   </button>
